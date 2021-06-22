7bbeb482f6419ce5c8c99efb055aa7b06ff8b1f4
dracero <diego.racero@gmail.com>
2021-06-22 20:52:51 -0300

    time

--- src/sigin.js
+++ src/sigin.js
@@ -45,6 +45,11 @@ class GoogleBtn extends Component {
       isLogined: false,
       accessToken: ""
     }));
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    console.log(dateTime)
     const { dispatch } = this.props;
     dispatch(logoutApp(this.state.isLogined));
   }
